c5e085be9b4192631803a967b7e17b907fa0a57d
aminmb37 <amin.mb37@gmail.com>
2020-11-19 22:08:29 +0100

    Configuring Gateway to use Eureka.

--- src/main/java/com/amin/gateway/config/LoadBalancedRouteConfig.java
+++ src/main/java/com/amin/gateway/config/LoadBalancedRouteConfig.java
@@ -7,16 +7,16 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("!local-discovery")
-public class LocalHostRouteConfig {
+@Profile("local-discovery")
+public class LoadBalancedRouteConfig {
     @Bean
-    public RouteLocator localHostRoutes(RouteLocatorBuilder routeLocatorBuilder) {
+    public RouteLocator loadBalancedRoutes(RouteLocatorBuilder routeLocatorBuilder) {
         return routeLocatorBuilder.routes()
                 .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
-                        .uri("http://localhost:8080").id("beer-service"))
+                        .uri("lb://beer-service").id("beer-service"))
                 .route(r -> r.path("/api/v1/customers/**")
-                        .uri("http://localhost:8081").id("order-service"))
+                        .uri("lb://beer-order-service").id("beer-order-service"))
                 .route(r -> r.path("/api/v1/beer/*/inventory")
-                        .uri("http://localhost:8082").id("inventory-service")).build();
+                        .uri("lb://beer-inventory-service").id("beer-inventory-service")).build();
     }
 }
